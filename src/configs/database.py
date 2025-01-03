a5db535cd9ad4113588cc930930318c00dbbea4a
r0bfp <robfpinheiro@gmail.com>
2025-01-03 09:20:19 -0300

    chore: ajusta database url

--- src/configs/database.py
+++ src/configs/database.py
@@ -4,17 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from os import environ
 
 
-
-connection_url = engine.url.URL.create(
-    drivername = 'cockroachdb',
-    username   = environ.get('RAGE_DB_USER'),
-    password   = environ.get('RAGE_DB_PASS'),
-    database   = environ.get('RAGE_DB_NAME'),
-    host       = environ.get('RAGE_DB_HOST'),
-    port       = 26257,
-    query      = {'sslmode': 'verify-full'}
-)
-engine = create_engine(connection_url)
+engine = create_engine(environ["DATABASE_URL"])
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
