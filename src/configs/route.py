a594ee2b6ff3c9333dfb938e492d03deb0707460
r0bfp <robfpinheiro@gmail.com>
2025-01-11 12:46:24 -0300

    fix: adjust route config trip .py bug

--- src/configs/route.py
+++ src/configs/route.py
@@ -7,7 +7,7 @@ def setup_routes(app: FastAPI) -> FastAPI:
     routes_path = 'src/routes'
 
     for file in Path(routes_path).glob('*.py'):
-        module_path = file.as_posix().replace('/', '.').strip('.py')
+        module_path = file.as_posix().replace('/', '.').replace('.py', '')
 
         router_module = importlib.import_module(module_path, 'router')
         app.include_router(router_module.router)
